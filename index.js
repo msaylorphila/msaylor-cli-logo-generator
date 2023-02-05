e7bc8b316f2e7706694827706bf2770ee9953711
msaylorphila <117216450+msaylorphila@users.noreply.github.com>
2023-02-05 13:15:19 -0500

    examples

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const { generateLogo }  = require('./lib/generate.js')
 const fs = require('fs/promises');
+const { Triangle } = require('./lib/shapes.js')
 
 const questions = [
     {
@@ -12,6 +13,17 @@ const questions = [
         type: 'input',
         message: 'Please enter a color or hexadecimal number for your text color choice',
         name: 'textColor',
+        default: 'white',
+        // validate: function (response){
+        //     if (response.includes('#')) {
+        //         const hexCheck = response.match(/[0-9A-Fa-f]{3}{1,2}$/)
+        //         if (hexCheck = null) {
+        //             return 'Please enter a valid hexidecimal number'
+        //         } 
+        //     return true
+        //     }
+        // }
+        
     },
     {
         type: 'list',
@@ -23,25 +35,43 @@ const questions = [
         type: 'input',
         message: 'Please enter a color or hexadecimal number for your shape color choice',
         name: 'shapeColor',
+        default: 'black',
+        // validate: function (response){
+        //     if (response.includes('#')) {
+        //         const hexCheck = response.match(/[0-9A-Fa-f]{3}{1,2}$/)
+        //         if (hexCheck = null) {
+        //             return 'Please enter a valid hexidecimal number'
+        //         } else {
+        //         return true
+        //         }
+        //     }
+        // }
     },
 ]
 
 const handleResponse = (response) => {
-    const logoSvg = generateLogo(response);
+    // const logoSvg = generateLogo(response);
+    // console.log(newTri.render())
 
     if (response.shape === "triangle"){
+    const newTri = new Triangle(response.text, response.textColor, response.shape, response.shapeColor)    
+    const logoSvg = newTri.render()
     fs.writeFile('./examples/triangle.svg', logoSvg, 'utf-8')
-        .then(() => console.log('Generated logo.svg'))
+        .then(() => console.log('Generated triangle.svg'))
         .catch(err => console.log`Error: ${err}`)
     } 
     else if (response.shape === "circle"){
+    const newCirc = new Circle(response.text, response.textColor, response.shape, response.shapeColor)
+    
         fs.writeFile('./examples/circle.svg', logoSvg, 'utf-8')
-            .then(() => console.log('Generated logo.svg'))
+            .then(() => console.log('Generated circle.svg'))
             .catch(err => console.log`Error: ${err}`)
     } 
     else if (response.shape === "square"){
+        const newSqua = new Square(response.text, response.textColor, response.shape, response.shapeColor)
+
             fs.writeFile('./examples/square.svg', logoSvg, 'utf-8')
-                .then(() => console.log('Generated logo.svg'))
+                .then(() => console.log('Generated square.svg'))
                 .catch(err => console.log`Error: ${err}`)
     }
 
@@ -53,3 +83,11 @@ inquirer
     .then((response) =>
      handleResponse(response)
     );
+
+// const newTri = new Triangle(response.text, response.textColor, response.shape, response.shapeColor)
+// const newTri = new Triangle
+
+// console.log(newTri.render())
+// module.exports = {
+//     response
+// }
